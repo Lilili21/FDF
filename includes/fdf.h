7d0213eece7e79770f41e2c0506d2f3e62d3fded
Lilili21 <42234833+Lilili21@users.noreply.github.com>
2019-07-07 16:38:38 +0300

    home_upgreat

--- includes/fdf.h
+++ includes/fdf.h
@@ -21,51 +21,88 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-typedef struct  s_map
-{
-	void				*mlx;
-	void				*win;
-	int					z;
-	int					width;
-	int					height;
-	int					linesizex;
-	int					linesizey;
-	int					count_strings;
-	int					*line;
-	unsigned long		color;
-	int					**tab;
-	int					x1;
-	int					x2;
-	int					y1;
-	int					y2;
-	int					dx;
-	int					dy;
-	int					incrx;
-	int					incry;
-	int					turn;
-	int					view_y;
-	int					view_x;
-}               t_map;
+typedef struct	s_map{
+    void		*mlx;
+    void		*win;
 
-void					draw_all_x(t_map *map);
-void					draw_all_y(t_map *map);
-void	                pixels(t_map *map);
-void					negative(t_map *map);
-void					positive(t_map *map);
-int						error(void);
-int						usage(void);
-t_map					work_coords(int fd, t_map *map);
-int						*assign_all_coords(char *line);
-void					ft_map(t_map *map);
-void					draw(t_map *map);
-int						check_line(char **tab);
-t_map					init_map(t_map *map);
-int						key_pressed(int keycode, t_map *map);
-int						exiterror(void);
-void					debugcoords(t_map map);
-int						tablen(char **tab);
-void	                count_strings(int fd, t_map *map);
-int                     error(void);
-int ft_close(void *param);
-//int                     mouse_pressed(int button, t_map *map);
+	void		*img;
+	int 		trash;
+	int *		data;
+    int			width;
+    int			height;
+}				t_map;
+
+typedef struct	s_prm{
+    int			start_x;
+    int			start_y;
+    int			linesizex;
+    int			linesizey;
+    int			zoom;
+
+	int			dx;
+	int			dy;
+	int			incrx;
+	int			incry;
+}               t_prm;
+
+typedef struct	s_xyz{
+    int			x;
+    int			y;
+    int			z;
+}				t_xyz;
+
+typedef struct	s_str
+{
+    t_prm		prm;
+    t_xyz		*xyz;
+    int			length; //count_elems/cout_strings
+	int			count_strings;
+    int			count_elems;
+
+	int			x1;
+	int			x2;
+	int			y1;
+	int			y2;
+
+	int			turn;
+	int			view_y;
+	int			view_x;
+}               t_str;
+
+/*
+ * main.c
+ */
+t_str			init_str(t_str *str, t_map *map);
+
+/*
+ * read_coordinates.c
+ */
+int				ft_count(int fd, t_str *str);
+void			work_coords(int fd, t_str *str);
+void			assign_all_coords(t_str *str, char *line, int num_line);
+
+/*
+ * utilise.c
+ */
+int				ft_str_len(char **tab);
+void			print_coords(t_str str);
+int				error(void);
+int				usage(void);
+
+/*
+ * Bresenham.c
+ */
+void	pixels(t_str *str, t_map *map, int i, int j);
+
+/*
+ * hook.c
+ */
+int                   	mouse_pressed(int button, t_str *str, t_map *map);
+int						key_pressed(int keycode, t_str *str, t_map *map);
+int                     ft_close(void *param);
+
+/*
+ * draw.c
+ */
+void			draw(t_str *str, t_map *map, int turn);
 #endif
