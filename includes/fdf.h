4a7ccd19f922945e31b021fc5264212e6ce0f5e6
Lilili21 <42234833+Lilili21@users.noreply.github.com>
2019-07-10 20:45:34 +0300

    upgreat

--- includes/fdf.h
+++ includes/fdf.h
@@ -13,6 +13,12 @@
 #ifndef FDF_H
 # define FDF_H
 
+#define PI 3.14159265359
+
+# define alfa (45.0)
+# define beta (0.0)
+# define gama (45.0)
+
 # include "libft.h"
 # include <math.h>
 # include "mlx.h"
@@ -21,65 +27,77 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-typedef struct	s_map{
-    void		*mlx;
-    void		*win;
-
-	void		*img;
-	int 		trash;
-	int *		data;
-    int			width;
-    int			height;
-}				t_map;
-
 typedef struct	s_prm{
-    int			start_x;
-    int			start_y;
-    int			linesizex;
-    int			linesizey;
-    int			zoom;
+int			start_x;
+int			start_y;
+int			linesizex;
+int			linesizey;
+int			zoom;
 
-	int			dx;
-	int			dy;
-	int			incrx;
-	int			incry;
+int			dx;
+int			dy;
+int			incrx;
+int			incry;
 }               t_prm;
 
 typedef struct	s_xyz{
-    int			x;
-    int			y;
-    int			z;
+	int			x;
+	int			y;
+	int			z;
 }				t_xyz;
+
+typedef struct	s_xyz2{
+	double			x;
+	double			y;
+	double			z;
+}				t_xyz2;
+
+typedef struct	s_map{
+
+	void		*img;
+	int 		*data;
+	void		*mlx;
+	void		*win;
+	int			width;
+	int			height;
+	int			width_img;
+	int			height_img;
+	int 		trash;
+
+}				t_map;
 
 typedef struct	s_str
 {
-    t_prm		prm;
-    t_xyz		*xyz;
-    int			length; //count_elems/cout_strings
+	t_map		map;
+	t_prm		prm;
+	t_xyz		*xyz;
+	t_xyz2		*xyz_tmp;
+	int			length;
 	int			count_strings;
-    int			count_elems;
+	int			count_elems;
 
-	int			x1;
-	int			x2;
-	int			y1;
-	int			y2;
+	double		delta_x;
+	double		delta_y;
+	double		x_alfa;
+	double		y_alfa;
+	double		z_alfa;
 
-	int			turn;
-	int			view_y;
-	int			view_x;
 }               t_str;
+
+
 
 /*
  * main.c
  */
-t_str			init_str(t_str *str, t_map *map);
+t_str			init_str(t_str *str);
 
 /*
  * read_coordinates.c
  */
 int				ft_count(int fd, t_str *str);
 void			work_coords(int fd, t_str *str);
-void			assign_all_coords(t_str *str, char *line, int num_line);
+//void			assign_all_coords(t_str *str, char *line, int num_line);
+void			assign_xy(t_str *str);
 
 /*
  * utilise.c
@@ -92,17 +110,25 @@ int				usage(void);
 /*
  * Bresenham.c
  */
-void	pixels(t_str *str, t_map *map, int i, int j);
+void	pixels(t_str *str, int i, int j);
 
 /*
  * hook.c
  */
-int                   	mouse_pressed(int button, t_str *str, t_map *map);
-int						key_pressed(int keycode, t_str *str, t_map *map);
+int mouse_pressed(int button, int x, int y, void *param);
+int key_pressed(int keycode, t_str *str);
 int                     ft_close(void *param);
 
 /*
  * draw.c
  */
-void			draw(t_str *str, t_map *map, int turn);
+void			draw(t_str *str, int turn);
+
+/*
+ * rotate.c
+ */
+void rotate_x(t_str *str, int i, int check);
+void rotate_y(t_str *str, int i, int check);
+void rotate_z(t_str *str, int i, int check);
+
 #endif
